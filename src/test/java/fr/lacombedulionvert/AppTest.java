6aa4c4c41c00db867e15a188a6377a0ee42a58a4
Lenovo 30 <lenovo30@lcdlv.fr>
2019-10-07 16:55:35 +0200

    Fraction first try

---
+++ src/test/java/fr/lacombedulionvert/AppTest.java
@@ -0,0 +1,24 @@
+package fr.lacombedulionvert;
+
+import static org.junit.Assert.assertTrue;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import java.util.stream.IntStream;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        Assertions.assertThat(true).isTrue();
+        IntStream i;
+    }
+}
