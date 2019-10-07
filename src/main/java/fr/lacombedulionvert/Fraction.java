ddee1c1cb16fc9fbf71e33a1ebe3059c814c396d
Lenovo 30 <lenovo30@lcdlv.fr>
2019-10-07 17:36:47 +0200

    First test 0 + 0

---
+++ src/main/java/fr/lacombedulionvert/Fraction.java
@@ -0,0 +1,10 @@
+package fr.lacombedulionvert;
+
+public class Fraction {
+    public Fraction(int i, int i1) {
+    }
+
+    public int add(Fraction f) {
+        return 0;
+    }
+}
--- src/test/java/fr/lacombedulionvert/AppTest.java
+++ src/test/java/fr/lacombedulionvert/AppTest.java
@@ -3,8 +3,10 @@ package fr.lacombedulionvert;
 import static org.junit.Assert.assertTrue;
 
 import org.assertj.core.api.Assertions;
+import org.junit.Assert;
 import org.junit.Test;
 
+import javax.swing.*;
 import java.util.stream.IntStream;
 
 /**
@@ -16,8 +18,17 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
+    public void shouldAnswerWithTrue() {
         Assertions.assertThat(false).isTrue();
     }
+
+    @Test
+    public void additionResultIsZero()
+    {
+        Fraction f = new Fraction(0, 1);
+
+        Assertions.assertThat(f.add(f)).isEqualTo(0);
+    }
+
+
 }
