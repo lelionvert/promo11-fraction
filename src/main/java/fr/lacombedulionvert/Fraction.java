cda82e7caa8fd2954f57ee8c3eb5e360962da0cb
Lenovo 30 <lenovo30@lcdlv.fr>
2019-10-09 11:49:42 +0200

    Add Fraction equals, toString Methods + Refactoring Tests

--- src/main/java/fr/lacombedulionvert/Fraction.java
+++ src/main/java/fr/lacombedulionvert/Fraction.java
@@ -1,5 +1,7 @@
 package fr.lacombedulionvert;
 
+import java.util.Objects;
+
 public class Fraction {
 
     private final int numerator;
@@ -20,5 +22,27 @@ public class Fraction {
 
     public int getDenominator() {
         return denominator;
+    }
+
+    @Override
+    public String toString() {
+        return "Fraction{" +
+                "numerator=" + numerator +
+                ", denominator=" + denominator +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Fraction fraction = (Fraction) o;
+        return numerator == fraction.numerator &&
+                denominator == fraction.denominator;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numerator, denominator);
     }
 }
--- src/test/java/fr/lacombedulionvert/FractionTest.java
+++ src/test/java/fr/lacombedulionvert/FractionTest.java
@@ -16,87 +16,74 @@ public class FractionTest {
 
     @Test
     public void add_0_and_0_equals_0() {
-        // Given
-        Fraction f = new Fraction(0, 1);
-
-        // When
-        Fraction result = f.add(f);
+        // Given / When
+        Fraction result = new Fraction(0, 1)
+                .add(new Fraction(0, 1));
 
         // Then
-        Assertions.assertThat(result.getNumerator()).isEqualTo(0);
-        Assertions.assertThat(result.getDenominator()).isEqualTo(1);
-
+        Assertions.assertThat(result).isEqualTo(
+                new Fraction(0, 1)
+        );
     }
 
     @Test
     public void add_0_and_1_equals_1() {
-        // Given
-        Fraction f0 = new Fraction(0, 1);
-        Fraction f1 = new Fraction(1, 1);
-
-        // When
-        Fraction result = f0.add(f1);
+        // Given / When
+        Fraction result = new Fraction(0, 1)
+                .add(new Fraction(1, 1));
 
         // Then
-        Assertions.assertThat(result.getNumerator()).isEqualTo(1);
-        Assertions.assertThat(result.getDenominator()).isEqualTo(1);
-
+        Assertions.assertThat(result).isEqualTo(
+                new Fraction(1, 1)
+        );
     }
 
     @Test
     public void add_3_and_2_equals_5() {
-        // Given
-        Fraction f3 = new Fraction(3, 1);
-        Fraction f2 = new Fraction(2, 1);
-
-        // When
-        Fraction result = f3.add(f2);
+        // Given / When
+        Fraction result = new Fraction(3, 1)
+                .add(new Fraction(2, 1));
 
         // Then
-        Assertions.assertThat(result.getNumerator()).isEqualTo(5);
-        Assertions.assertThat(result.getDenominator()).isEqualTo(1);
-
+        Assertions.assertThat(result).isEqualTo(
+                new Fraction(5, 1)
+        );
     }
 
     @Test
     public void add_1_on_2_and_3_on_2_equals_4_on_2() {
-        // Given
-        Fraction f1_2 = new Fraction(1, 2);
-        Fraction f3_2 = new Fraction(3, 2);
-
-        // When
-        Fraction result = f1_2.add(f3_2);
+        // Given / When
+        Fraction result = new Fraction(1, 2)
+                .add(new Fraction(3, 2));
 
         // Then
-        Assertions.assertThat(result.getNumerator()).isEqualTo(4);
-        Assertions.assertThat(result.getDenominator()).isEqualTo(2);
+        Assertions.assertThat(result).isEqualTo(
+                new Fraction(4, 2)
+        );
     }
 
     @Test
     public void add_1_on_5_and_3_on_5_equals_4_on_5() {
-        // Given
-        Fraction f1_5 = new Fraction(1, 5);
-        Fraction f3_5 = new Fraction(3, 5);
-
-        // When
-        Fraction result = f1_5.add(f3_5);
+        // Given / When
+        Fraction result = new Fraction(1, 5)
+                .add(new Fraction(3, 5));
 
         // Then
-        Assertions.assertThat(result.getNumerator()).isEqualTo(4);
-        Assertions.assertThat(result.getDenominator()).isEqualTo(5);
+        Assertions.assertThat(result).isEqualTo(
+                new Fraction(4, 5)
+        );
     }
 
     @Test
     public void add_1_on_3_and_1_on_3_equals_2_on_3() {
-        // Given
-        Fraction f1_3 = new Fraction(1, 3);
-
-        // When
-        Fraction result = f1_3.add(f1_3);
+        // Given / When
+        Fraction result = new Fraction(1, 3)
+                .add(new Fraction(1, 3));
 
         // Then
-        Assertions.assertThat(result.getNumerator()).isEqualTo(2);
-        Assertions.assertThat(result.getDenominator()).isEqualTo(3);
+        Assertions.assertThat(result).isEqualTo(
+                new Fraction(2, 3)
+        );
     }
 
 }
